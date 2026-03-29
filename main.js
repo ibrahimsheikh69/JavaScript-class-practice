cc4271563d72bd05b1f005c18ed3c9c54a06ea09
Ibrahim <m.ibrahimsheikh2006@gmail.com>
2026-03-29 18:50:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// program 1
+
+console.log("Program 1");
+
+const na = "Ibrahim Sheikh";
+let country = "Pakistan";
+let city = "Hyderabad";
+let age = 18;
+
+console.log("Before Update:")
+console.log("Name = " + na);
+console.log("Citizen of " + country);
+console.log("lives in " + city);
+console.log("age is " + age);
+
+country = "Japan";
+city = "Okinawa";
+age = 26;
+
+console.log("After Update:")
+console.log("Name = " + na);
+console.log("Citizen of " + country);
+console.log("lives in " + city);
+console.log("age is " + age);
+
+// Program 2
+
+console.log("Program 2");
+
+let name1 = prompt("hey tell me your name");
+let age1 = prompt("hey tell me your age");
+
+console.log(`Hello, ${name1} You are ${age1} years old.`)
+alert(`Hello, ${name1} You are ${age1} years old.`)
+
